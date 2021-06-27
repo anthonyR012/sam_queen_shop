6caf87805df9516a3e3ee60fa35506c1efef802a
anthonyR012 <rubionn27@gmail.com>
2021-06-27 10:17:57 -0500

    Feat: implements animation

--- ts/app.js
+++ ts/app.js
@@ -1,1 +1,24 @@
-console.log("si compila bien");
+// variables
+var aplicationAnimacionScroll = document.querySelector("nav.navbar-expand-sm");
+// inicio app
+window.addEventListener("scroll", function () {
+    var divAfer = document.querySelector(".scroll-css");
+    var ubication = divAfer.getBoundingClientRect();
+    if (ubication.top < 100) {
+        changeAnimation(true);
+    }
+    else {
+        changeAnimation(false);
+    }
+});
+// funciones
+function changeAnimation(valorDeEntrada) {
+    if (valorDeEntrada === true) {
+        aplicationAnimacionScroll.classList.add("transicionColorNav-css");
+    }
+    else {
+        if (aplicationAnimacionScroll.className === "transicionColorNav-css") {
+            aplicationAnimacionScroll.remove();
+        }
+    }
+}
--- ts/app.ts
+++ ts/app.ts
@@ -1,1 +1,31 @@
-console.log("si compila bien");
+
+// variables
+const aplicationAnimacionScroll : Element = document.querySelector("nav.navbar-expand-sm");
+
+
+
+// inicio app
+window.addEventListener("scroll" ,()=>{
+  const divAfer :Element = document.querySelector(".scroll-css");
+  const ubication: DOMRect = divAfer.getBoundingClientRect();
+    if(ubication.top < 100){
+        changeAnimation(true);
+    }else{
+        changeAnimation(false);
+    }
+
+})
+
+
+// funciones
+    function  changeAnimation(valorDeEntrada: boolean):void {
+        if(valorDeEntrada===true){
+        aplicationAnimacionScroll.classList.add("transicionColorNav-css");
+        }else{
+            if(aplicationAnimacionScroll.className==="transicionColorNav-css"){
+                aplicationAnimacionScroll.remove();
+            }
+        }
+        
+    }
+
