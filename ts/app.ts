c42196a1bf3ddea87113a9d289be0f1235a0fdd9
anthonyR012 <rubionn27@gmail.com>
2021-06-28 11:47:29 -0500

    feat-implement: tsConfig, sass incorporation

--- ts/app.ts
+++ ts/app.ts
@@ -6,31 +6,32 @@
 
 // inicio app
 window.addEventListener("scroll" ,()=>{
-  const divAfer :Element = document.querySelector(".scroll-css");
-  const ubication: DOMRect = divAfer.getBoundingClientRect();
-    if(ubication.top < 100){
-        changeAnimation(true);
-    }else{
-        changeAnimation(false);
-    }
-
-})
-
-
-// funciones
-    function  changeAnimation(valorDeEntrada: boolean):void {
-        const aplicationAnimacionScroll : Element = document.querySelector("nav.navbar-expand-sm");
-        if(valorDeEntrada===true){
-           
-        aplicationAnimacionScroll.classList.add("transicionAfterNav-css");
-
-        }else{
-            if(aplicationAnimacionScroll.classList.contains("transicionAfterNav-css")){
-                
-                aplicationAnimacionScroll.classList.remove("transicionAfterNav-css");
-                aplicationAnimacionScroll.classList.add("transicionBeforeNav-css");
-            }
-        }
-        
-    }
-
+    const divAfer :Element|null = document.querySelector(".scroll-css");
+    const ubication: DOMRect = divAfer!!.getBoundingClientRect();
+      if(ubication.top < 100){
+          changeAnimation(true);
+      }else{
+          changeAnimation(false);
+      }
+  
+  })
+  
+  
+  // funciones
+      function  changeAnimation(valorDeEntrada: boolean):void {
+          const aplicationAnimacionScroll : Element = document.querySelector("nav.navbar-expand-sm");
+          if(valorDeEntrada===true){
+             
+          aplicationAnimacionScroll.classList.add("transicionAfterNav-css");
+  
+          }else{
+              if(aplicationAnimacionScroll.classList.contains("transicionAfterNav-css")){
+                  
+                  aplicationAnimacionScroll.classList.remove("transicionAfterNav-css");
+                  aplicationAnimacionScroll.classList.add("transicionBeforeNav-css");
+              }
+          }
+          
+      }
+  
+  
