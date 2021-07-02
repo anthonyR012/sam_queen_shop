4e62c0ef1e986ce4aa89c3e86498b0f806282258
anthonyR012 <rubionn27@gmail.com>
2021-07-02 10:45:11 -0500

    Change Category's icons and improve the animation

--- ts/app.ts
+++ ts/app.ts
@@ -6,7 +6,7 @@
 // INICIO
 eventListners();
 function eventListners(){
-  window.addEventListener("scroll", positionScroll);
+
   
 
 
@@ -18,35 +18,6 @@ function eventListners(){
 
 
 
-function positionScroll(){    
-  const divAfer :Element|null = document.querySelector(".scroll-css");
-  const ubication: DOMRect = divAfer!!.getBoundingClientRect();
-        if(ubication.top < 100){
-          
-          animationHeader(true);
-        }else{
-          animationHeader(false);
-        }
-  
-    }
-  
-function  animationHeader(valorDeEntrada: boolean):void {
-    const aplicationAnimacionScroll : Element|null = document.querySelector("nav.navbar-expand-sm");
-
-      if(valorDeEntrada===true && aplicationAnimacionScroll){
-        
-      aplicationAnimacionScroll.classList.add("transicionAfterNav-css");
-
-      }else if(aplicationAnimacionScroll){
-          if(aplicationAnimacionScroll.classList.contains("transicionAfterNav-css")){
-              
-              aplicationAnimacionScroll.classList.remove("transicionAfterNav-css");
-              aplicationAnimacionScroll.classList.add("transicionBeforeNav-css");
-          }
-      }
-      
-}
-  
 
 
 
