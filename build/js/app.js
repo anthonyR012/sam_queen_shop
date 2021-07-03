e95a98f1a14098abd6c41b2b5d618f27fb5656c2
anthonyR012 <rubionn27@gmail.com>
2021-07-03 11:22:57 -0500

    Feat: animation change at the Category

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,40 @@
 "use strict";
 // VARIABLES
+var aplicationAnimacionScrollCategory = document.querySelector(".sticky-top");
 // INICIO
 eventListners();
 function eventListners() {
+    window.addEventListener("scroll", positionScroll);
 }
 // FUNCIONES
+function positionScroll() {
+    var ubication = aplicationAnimacionScrollCategory.getBoundingClientRect();
+    if (ubication.top < 110) {
+        animationHeader(true);
+    }
+    else {
+        animationHeader(false);
+    }
+}
+function animationHeader(valorDeEntrada) {
+    var aplicationAnimacionScrollHeader = document.querySelector(".navbarPosition-css");
+    if (valorDeEntrada === true && aplicationAnimacionScrollHeader) {
+        if (!(aplicationAnimacionScrollCategory === null || aplicationAnimacionScrollCategory === void 0 ? void 0 : aplicationAnimacionScrollCategory.classList.contains("transitionBefore-cs"))) {
+            aplicationAnimacionScrollHeader.classList.add("transitionBefore-css");
+            aplicationAnimacionScrollCategory === null || aplicationAnimacionScrollCategory === void 0 ? void 0 : aplicationAnimacionScrollCategory.classList.add("transitionAfter-css");
+        }
+        else {
+            aplicationAnimacionScrollHeader === null || aplicationAnimacionScrollHeader === void 0 ? void 0 : aplicationAnimacionScrollHeader.classList.remove("withoutAnimation-css");
+            aplicationAnimacionScrollCategory === null || aplicationAnimacionScrollCategory === void 0 ? void 0 : aplicationAnimacionScrollCategory.classList.remove("withoutAnimation-css");
+            aplicationAnimacionScrollCategory === null || aplicationAnimacionScrollCategory === void 0 ? void 0 : aplicationAnimacionScrollCategory.classList.remove("transitionAfter-css");
+        }
+    }
+    else if (aplicationAnimacionScrollHeader) {
+        if (aplicationAnimacionScrollHeader.classList.contains("transitionBefore-css")) {
+            aplicationAnimacionScrollHeader === null || aplicationAnimacionScrollHeader === void 0 ? void 0 : aplicationAnimacionScrollHeader.classList.remove("transitionBefore-css");
+            aplicationAnimacionScrollCategory === null || aplicationAnimacionScrollCategory === void 0 ? void 0 : aplicationAnimacionScrollCategory.classList.remove("transitionAfter-css");
+            aplicationAnimacionScrollHeader === null || aplicationAnimacionScrollHeader === void 0 ? void 0 : aplicationAnimacionScrollHeader.classList.add("withoutAnimation-css");
+            aplicationAnimacionScrollCategory === null || aplicationAnimacionScrollCategory === void 0 ? void 0 : aplicationAnimacionScrollCategory.classList.add("withoutAnimation-css");
+        }
+    }
+}
--- ts/app.ts
+++ ts/app.ts
@@ -1,23 +1,60 @@
 // VARIABLES
-
-
-
+const aplicationAnimacionScrollCategory :Element|null = document.querySelector(".sticky-top");
 
 // INICIO
 eventListners();
 function eventListners(){
-
-  
+ 
+ window.addEventListener("scroll", positionScroll);
 
 
 
   
 }
   // FUNCIONES
+  function positionScroll(){    
+
+    const ubication: DOMRect = aplicationAnimacionScrollCategory!!.getBoundingClientRect();
+          if(ubication.top < 110){
+            
+            animationHeader(true);
+          }else{
+           
+            animationHeader(false);
+          }
+    
+      }
 
 
+      function  animationHeader(valorDeEntrada: boolean):void {
+        const aplicationAnimacionScrollHeader : Element|null = document.querySelector(".navbarPosition-css");
+        
+          if(valorDeEntrada===true && aplicationAnimacionScrollHeader){
+           
+            if(!aplicationAnimacionScrollCategory?.classList.contains("transitionBefore-cs")){
+              aplicationAnimacionScrollHeader.classList.add("transitionBefore-css");
+              aplicationAnimacionScrollCategory?.classList.add("transitionAfter-css");
+            }else{
+              aplicationAnimacionScrollHeader?.classList.remove("withoutAnimation-css");
+              aplicationAnimacionScrollCategory?.classList.remove("withoutAnimation-css");
+              aplicationAnimacionScrollCategory?.classList.remove("transitionAfter-css");
+            }
+         
 
-
+    
+    
+          }else if(aplicationAnimacionScrollHeader){
+              if(aplicationAnimacionScrollHeader.classList.contains("transitionBefore-css")){
+           
+                aplicationAnimacionScrollHeader?.classList.remove("transitionBefore-css");
+                  aplicationAnimacionScrollCategory?.classList.remove("transitionAfter-css");
+                  aplicationAnimacionScrollHeader?.classList.add("withoutAnimation-css");
+                  aplicationAnimacionScrollCategory?.classList.add("withoutAnimation-css");
+                  
+              }
+          }
+          
+    }
 
 
 
