820839875440dd550cccb135e85847809e20bb0c
anthonyR012 <rubionn27@gmail.com>
2021-07-04 19:04:25 -0500

    Feat: Reduction code and details

--- build/js/app.js
+++ build/js/app.js
@@ -1,12 +1,40 @@
 "use strict";
 // VARIABLES
 var aplicationAnimacionScrollCategory = document.querySelector(".sticky-top");
+var colorClickDesplegable = document.querySelector(".activeDesplegable-css");
 // INICIO
 eventListners();
 function eventListners() {
+    colorClickDesplegable === null || colorClickDesplegable === void 0 ? void 0 : colorClickDesplegable.addEventListener("click", clickAtDesplegable);
     window.addEventListener("scroll", positionScroll);
 }
 // FUNCIONES
+function clickAtDesplegable() {
+    var objectColorHeader = document.querySelector(".navbarPosition-css");
+    var colorFondo = window.getComputedStyle(objectColorHeader).backgroundColor;
+    if (objectColorHeader && colorFondo == "rgba(0, 0, 0, 0)") {
+        objectColorHeader.setAttribute("style", "background-color:#266b76;transition-duration:1s");
+        colorClickDesplegable === null || colorClickDesplegable === void 0 ? void 0 : colorClickDesplegable.classList.add("disabled");
+        sloganOpacoHeader(1);
+        setTimeout(function () {
+            colorClickDesplegable === null || colorClickDesplegable === void 0 ? void 0 : colorClickDesplegable.classList.remove("disabled");
+            console.log(colorClickDesplegable);
+        }, 1000);
+    }
+    else if (objectColorHeader) {
+        objectColorHeader.setAttribute("style", "background-color:none;transition-duration:0.5s");
+        sloganOpacoHeader(0);
+    }
+}
+function sloganOpacoHeader(param) {
+    var slogan = document.querySelector(".containerSlogan-css");
+    if (param == 1 && slogan) {
+        slogan.setAttribute("style", "opacity:0;transition-duration:0.2s");
+    }
+    else if (slogan) {
+        slogan.setAttribute("style", "opacity:100;transition-duration:0.5s");
+    }
+}
 function positionScroll() {
     var ubication = aplicationAnimacionScrollCategory.getBoundingClientRect();
     if (ubication.top < 110) {
--- ts/app.ts
+++ ts/app.ts
@@ -1,10 +1,11 @@
 // VARIABLES
 const aplicationAnimacionScrollCategory :Element|null = document.querySelector(".sticky-top");
+const colorClickDesplegable = document.querySelector(".activeDesplegable-css");
 
 // INICIO
 eventListners();
 function eventListners(){
- 
+ colorClickDesplegable?.addEventListener("click", clickAtDesplegable);
  window.addEventListener("scroll", positionScroll);
 
 
@@ -12,6 +13,40 @@ function eventListners(){
   
 }
   // FUNCIONES
+
+  function clickAtDesplegable(){
+    const objectColorHeader :Element|null = document.querySelector(".navbarPosition-css");
+    const colorFondo = window.getComputedStyle(objectColorHeader!!).backgroundColor;
+
+    if(objectColorHeader && colorFondo == "rgba(0, 0, 0, 0)"){
+        objectColorHeader.setAttribute("style", "background-color:#266b76;transition-duration:1s");
+
+        colorClickDesplegable?.classList.add("disabled");
+        sloganOpacoHeader(1);
+        setTimeout(() => {
+          colorClickDesplegable?.classList.remove("disabled");
+          console.log(colorClickDesplegable)
+        }, 1000);
+
+
+    }else if(objectColorHeader){
+      objectColorHeader.setAttribute("style", "background-color:none;transition-duration:0.5s");
+      sloganOpacoHeader(0);
+    }
+    
+
+  }
+
+
+  function sloganOpacoHeader(param: number){
+    const slogan:Element|null = document.querySelector(".containerSlogan-css");
+    if(param == 1 && slogan){
+      slogan.setAttribute("style", "opacity:0;transition-duration:0.2s");
+    }else if(slogan){
+      slogan.setAttribute("style", "opacity:100;transition-duration:0.5s");
+    }
+  }
+
   function positionScroll(){    
 
     const ubication: DOMRect = aplicationAnimacionScrollCategory!!.getBoundingClientRect();
